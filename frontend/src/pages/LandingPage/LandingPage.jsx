7c26f289f3819a2072f66714c06ccd57b11b7f7e
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-07-18 21:15:02 +0530

    Update AdminLogin and LandingPage components

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -10,34 +10,36 @@ import Footer from "../../components/Footer/Footer";
 function LandingPage() {
   return (
     <>
-      <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Container style={{ color: "white" }}>
-        <Row style={{ paddingLeft: "120px" }}>
-          <Col style={{ fontSize: "35px" }}>
-            <i>GREEN ENERGY </i>
-            <br />
-            <i style={{ paddingLeft: "90px" }}>For a</i>
-            <br />
-            <i style={{ paddingLeft: "50px" }}>BETTER TOMORROW</i>
-          </Col>
-        </Row>
-      </Container>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Footer />
+      <div className="bg">
+        <Header />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Container style={{ color: "white" }}>
+          <Row style={{ paddingLeft: "120px" }}>
+            <Col style={{ fontSize: "35px" }}>
+              <i>GREEN ENERGY </i>
+              <br />
+              <i style={{ paddingLeft: "90px" }}>For a</i>
+              <br />
+              <i style={{ paddingLeft: "50px" }}>BETTER TOMORROW</i>
+            </Col>
+          </Row>
+        </Container>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Footer />
+      </div>
     </>
   );
 }
